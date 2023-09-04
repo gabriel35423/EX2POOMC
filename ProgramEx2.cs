fd8491847d7dcc8cdbcbd1a195a8ca06208e967d
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 18:54:04 -0300

    Add files via upload

---
+++ ProgramEx2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2POOMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da aresta do quadrado:");
+
+            Double valorAresta = Double.Parse(Console.ReadLine());
+
+
+
+            Aresta quadrado = new Aresta(valorAresta);
+
+
+
+            Double area = quadrado.CalcularArea();
+
+            Console.WriteLine("A área do quadrado é: {0}", area);
+        }
+    }
+}
